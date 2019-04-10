6cc58a78eca318a92a8e8f116da474f2ac7f584a
Dawid Margraf <David_neo1@wp.pl>
2019-04-10 18:20:47 +0200

    Add loop

--- script.js
+++ script.js
@@ -5,42 +5,71 @@
         {
             id: "box1",
             title: "First box",
-            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            content: "Lorem ipsum dolor sit amet!1",
             categories: ["highlighted", "special-header", "important"]
         },
         {
             id: "box2",
             title: "Second box",
-            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            content: "Lorem ipsum dolor sit amet!2",
             categories: ["special-header", "important"]
         },
         {
             id: "box3",
             title: "Third box",
-            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            content: "Lorem ipsum dolor sit amet!3",
             categories: ["highlighted", "important"]
         },
         {
             id: "box4",
             title: "Fourth box",
-            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            content: "Lorem ipsum dolor sit amet!4",
             categories: ["highlighted"]
         },
         {
             id: "box5",
             title: "Fifth box",
-            content: "<p>Lorem ipsum dolor sit amet!</p>",
-            categories: []
+            content: "Lorem ipsum dolor sit amet!5",
+            categories: [""]
         }
     ];
    
 
-for (let i=0; i<data.length; i++) {
-    console.log( data[i] );
-}
-var div = document.createElement("div");
-var header = document.createElement("header");
-var headerContent = document.createTextNode("title");
-header.appendChild(headerContent);
-div.append(header);
+    for (let i = 0; i < data.length; i++) {
+        
+        var currentArrayElement = data[i]; 
+        var header = document.createElement('header');
+        var div = document.createElement('div');
+        div.setAttribute('id', currentArrayElement.id);
+        
+        for (let j = 0; j < currentArrayElement.categories.length; j++) {   
+            var currentCategories = currentArrayElement.categories[j];  
+        
+            console.log('1', currentCategories);
+            div.setAttribute('class', currentCategories);
+            
+        }
+    
+        
+        var headerContent = document.createTextNode(currentArrayElement.title);
+    
+        var p = document.createElement('p');
+        p.innerHTML = currentArrayElement.content;
+    
+        header.appendChild(headerContent);
+        div.appendChild(header);
+        div.appendChild(p);
+        document.body.appendChild(div);
+    }
+
 })();
+//div.classList.add('highlighted');
+        //div.setAttribute('class', currentCategories);
+        //header.setAttribute('class', currentCategories);
+        //zamiast join() -> przeleciec manualnie po calej tablicy `categories` i dla kazdego
+        //jej elementu (czyli klasy) wykonac div.classList.add(klasa);
+    
+       
+        //header.classList.add('special-header');
+
+       
